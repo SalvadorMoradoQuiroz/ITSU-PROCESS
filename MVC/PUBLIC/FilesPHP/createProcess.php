bbb1cf5071d1d96454168da083f65c5d2e471e5c
Salvador Morado <salvadormorado2000@gmail.com>
2022-05-29 20:52:53 -0500

    Se agrego logica para crear proceso

--- MVC/PUBLIC/FilesPHP/createProcess.php
+++ MVC/PUBLIC/FilesPHP/createProcess.php
@@ -7,12 +7,55 @@ session_start();
 
 $title = $_POST['title'];
 $description = $_POST['description'];
-$video = $_POST['video'];
-$image = $_POST['image'];
-$document = $_POST['doc'];
+$document = $_FILES['doc']['name'];
+$video = $_FILES['video']['name'];
+$image = $_FILES['image']['name'];
 $department = $_SESSION['departamento'];
 
-$objProcessController = new ProcessController();
-echo json_encode($objProcessController->altaProcess($title, $description, $video, $image, $document, $department ));
+$flagDoc = false;
+$flagVideo = false;
+$flagImage = false;
 
-?>
+$rutaDocs = '../../PUBLIC/DATA/DOCS/';
+$rutaFileDoc = $rutaDocs . $document;
+if (move_uploaded_file($_FILES['doc']['tmp_name'], $rutaFileDoc)) {
+    //echo "<script>alert('El documento se subió correctamente');</script>";
+    $flagDoc = true;
+} else {
+    $flagDoc = false;
+}
+
+if (!empty($video)) {
+    $rutaVideos = '../../PUBLIC/DATA/VIDEOS/';
+    $rutaFileVideo = $rutaVideos . $video;
+    if (move_uploaded_file($_FILES['video']['tmp_name'], $rutaFileVideo)) {
+        //echo "<script>alert('El video se subió correctamente');</script>";
+        $flagVideo = true;
+    } else {
+        $flagVideo = false;
+    }
+} else {
+    $video = null;
+    $flagVideo = true;
+}
+
+if (!empty($image)) {
+    $rutaImages = '../../PUBLIC/DATA/IMAGES/';
+    $rutaFileImage = $rutaImages . $image;
+    if (move_uploaded_file($_FILES['image']['tmp_name'], $rutaFileImage)) {
+        //echo "<script>alert('La imagen se subió correctamente');</script>";
+        $flagImage = true;
+    } else {
+        $flagImage = false;
+    }
+} else {
+    $image = null;
+    $flagImage = true;
+}
+
+if ($flagDoc && $flagVideo && $flagImage) {
+    $objProcessController = new ProcessController();
+    echo json_encode($objProcessController->altaProcess($title, $description, $video, $image, $document, $department));
+} else {
+    echo json_encode('Algún archivo no se subió correctamente, intente de nuevo.');
+}
--- MVC/PUBLIC/JS/createProcessJS.js
+++ MVC/PUBLIC/JS/createProcessJS.js
@@ -2,20 +2,36 @@ $(document).ready(function () {
     $('#formCreateProcess').submit(function (ob) {
         ob.preventDefault();
 
-        console.log($(this).serialize());
-            $.ajax({
-                type: 'POST',
-                url: '/ITSU-PROCESS/MVC/PUBLIC/FilesPHP/createProcess.php',
-                data: $(this).serialize(),
-                //type:'text/json',
-                success: function (response) {
-                    var jsonData = JSON.parse(response);
-                    console.log(jsonData);
-                    alert(jsonData);
-                    if (jsonData.toString() === 'Proceso registrado con éxito!') {
-                        //window.location.href = '../../MVC/VIEWS/login.html';
+        if ($('#title').val().length >= 5 && $('#title').val().length <= 50) {
+            if ($('#description').val().length >= 20 && $('#description').val().length <= 700) {
+
+
+                var formData = new FormData(document.getElementById("formCreateProcess"));
+                console.log(formData.toString());
+                $.ajax({
+                    type: 'POST',
+                    url: '/ITSU-PROCESS/MVC/PUBLIC/FilesPHP/createProcess.php',
+                    dataType: "html",
+                    data: formData,
+                    cache: false,
+                    contentType: false,
+                    processData: false,
+                    success: function (response) {
+                        var jsonData = JSON.parse(response);
+                        console.log(jsonData);
+                        alert(jsonData);
+                        if (jsonData.toString() === 'Proceso registrado con éxito!') {
+                            window.location.href = '../../MVC/VIEWS/admin.php';
+                        }
                     }
-                }
-            });
+                });
+
+
+            } else {
+                alert('La descripción debe de tener como mínimo 20 caracteres y como máximo 700.');
+            }
+        } else {
+            alert('El título debe de tener como mínimo 5 caracteres y como máximo 50.');
+        }
     });
 });
